611c68291fcdb86cac690a835379497b7a247d65
Omar Bowen <bronxkidd95@gmail.com>
2022-04-08 19:12:14 -0700

    Completed Api Endpoints

--- spec/requests/cats_request_spec.rb
+++ spec/requests/cats_request_spec.rb
@@ -11,36 +11,36 @@ RSpec.describe "Cats", type: :request do
       )
 
       # Make a request
-      get '/cats'
+          get '/cats'
 
-      cat = JSON.parse(response.body)
-      expect(response).to have_http_status(200)
-      expect(cat.length).to eq 1
-    end
-    describe "POST /create" do
-  it "creates a cat" do
+          cat = JSON.parse(response.body)
+          expect(response).to have_http_status(200)
+          expect(cat.length).to eq 1
+        end
+  describe "POST /create" do
+    it "creates a cat" do
     # The params we are going to send with the request
-    cat_params = {
-      cat: {
-        name: 'Buster',
-        age: 4,
-        enjoys: 'Meow Mix, and plenty of sunshine.',
-        image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
-      }
-    }
+      cat_params = {
+        cat: {
+          name: 'Buster',
+          age: 4,
+          enjoys: 'Meow Mix, and plenty of sunshine.',
+          image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+          }
+        }
 
     # Send the request to the server
-    post '/cats', params: cat_params
+        post '/cats', params: cat_params
 
     # Assure that we get a success back
-    expect(response).to have_http_status(200)
+        expect(response).to have_http_status(200)
 
     # Look up the cat we expect to be created in the db
-    cat = Cat.first
+        cat = Cat.first
 
     # Assure that the created cat has the correct attributes
-    expect(cat.name).to eq 'Buster'
-  end
-  end
-end
-end 
+        expect(cat.name).to eq 'Buster'
+      end
+      end
+    end
+    end
